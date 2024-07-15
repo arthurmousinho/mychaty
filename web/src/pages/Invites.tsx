a20749828ed7b5102d685a76216b0cd69d5c660e
arthurmousinho <arthursilvamo296@gmail.com>
2024-07-15 08:34:40 -0300

    feat: add filter invites component

--- web/src/pages/Invites.tsx
+++ web/src/pages/Invites.tsx
@@ -3,6 +3,14 @@ import { NewInviteDialog } from "@/components/invite/NewInviteDialog";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
 const invites = [
     {
         name: 'Diego Fernandes',
@@ -31,12 +39,33 @@ export function Invites() {
                 <h1 className="text-2xl font-semibold">
                     Manage Your Invites
                 </h1>
-                <NewInviteDialog>
-                    <Button className="flex items-center gap-2">
-                        <Plus size={20} />
-                        Send Invite
-                    </Button>
-                </NewInviteDialog>
+                <div className="flex items-center gap-4">   
+                    <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Filter invites" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem 
+                                value="received" 
+                                className="cursor-pointer"
+                            >
+                                Received Invites
+                            </SelectItem>
+                            <SelectItem 
+                                value="sent" 
+                                className="cursor-pointer"
+                            >
+                                Sent Invites
+                            </SelectItem>
+                        </SelectContent>
+                    </Select>
+                    <NewInviteDialog>
+                        <Button className="flex items-center gap-2">
+                            <Plus size={20} />
+                            Send Invite
+                        </Button>
+                    </NewInviteDialog>
+                </div>
             </header>
             <div className="grid grid-cols-3 gap-4">
                 { invites.map(invite => <InviteCard name={invite.name} />) }
